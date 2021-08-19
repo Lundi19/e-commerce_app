fd7f6e70485f37f299c3f06f28fab3c189853001
Michael McLoughlin <80280306+Lundi19@users.noreply.github.com>
2021-08-19 20:15:09 +0200

    homepage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
+import React from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+  return  <HomePage></HomePage>
+
 }
 
 export default App;
