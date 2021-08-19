d804564d6bfa08c02ed12c3833fda412f98e7b9c
Michael McLoughlin <80280306+Lundi19@users.noreply.github.com>
2021-08-19 22:04:01 +0200

    refactor and resize menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './menu-item.styles.scss'
 
-const MenuItem = ({title}) => (
+const MenuItem = ({title, imageUrl, size}) => (
 
-  <div className= 'menu-item'>
-  <div className= 'content'> 
-  <h1 className= 'title' >{title}</h1>
-  <span className= 'subtitle' >SHOP NOW</span>
+  <div 
+  style={{
+    backgroundImage: `url(${imageUrl})`
+  }} className={`${size} menu-item`}
+  >
+    <div className= 'content'> 
+      <h1 className= 'title' >{title}</h1>
+        <span className= 'subtitle' >SHOP NOW</span>
+    </div>
   </div>
-</div>
 )
 
 export default MenuItem
